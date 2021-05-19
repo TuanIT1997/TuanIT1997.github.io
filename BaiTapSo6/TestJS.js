69c3d8cc4ea046e994abaaac1fc1d39e11ebac02
TuanIT1997 <tranluuquoctuan1997@gmail.com>
2021-05-19 18:41:49 +0700

    BaiTapSo6

---
+++ BaiTapSo6/TestJS.js
@@ -0,0 +1,17 @@
+let a = ["hi",3,1.6,null,5]
+console.log(typeof(a[0]));
+console.log(typeof(a[1]));
+console.log(typeof(a[2]));
+console.log(typeof(a[3]));
+console.log(typeof(a[4]));
+///////////////////////
+if (a[4]%2==0){
+    console.log(`so chan`);
+}else console.log(`so le`);
+////////////////
+let info = {
+    name:"Tuan",
+    age: 24,
+    birth: `15/05/1998`,
+}
+console.log(`Xin chào, tên tôi là `,info.name,` nam nay tôi `,info.age,` tuổi, rất vui được làm quen`);
